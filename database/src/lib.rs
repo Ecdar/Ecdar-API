0a090ef3efdb6738ff974e5e9216354269afa337
williamwoldum <wwoldu21@student.aau.dk>
2023-10-13 15:16:58 +0200

    Small env changes

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -20,13 +20,12 @@ pub trait EcdarDatabase {
 impl EcdarDatabase for DatabaseContext {
     async fn new() -> Result<DatabaseContext, DbErr> {
         let database_url = env::var("DATABASE_URL").expect("Expected DATABASE_URL to be set.");
-        let db_name = env::var("DB_NAME").expect("Expected DB_NAME to be set.");
 
         let db = Database::connect(database_url.clone()).await?;
 
         match db.get_database_backend() {
             DbBackend::Postgres => {
-                let url = format!("{}/{}", database_url.clone(), db_name.clone());
+                let url = format!("{}", database_url.clone());
                 Database::connect(&url).await?
             }
             _ => {
