d5efda2797fdfa0b4e35d6f45f60e0da594683f6
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-10-16 15:33:08 +0200

    added tests to userContext

--- src/tests/database/user_context.rs
+++ src/tests/database/user_context.rs
@@ -1,10 +1,39 @@
 use crate::database::user_context;
+use crate::database::database_context::DatabaseContext;
+use crate::database::entity_context::EntityContextTrait;
+use crate::entities::prelude::User;
+use crate::entities::user::{ActiveModel, Model};
 
 #[cfg(test)]
 mod database_tests {
+    use sea_orm::{
+        entity::prelude::*, entity::*, tests_cfg::*,
+        DatabaseBackend, MockDatabase, Transaction,
+    };
+    use crate::entities::user;
 
-    #[test]
-    fn create_test() -> () {
+    #[tokio::test]
+    async fn create_test() -> Result<(),DbErr> {
+        let db = MockDatabase::new(DatabaseBackend::Postgres).append_query_results([
+            vec![user::Model{
+                id: 1,
+                email: "anders21@student.aau.dk".to_owned(),
+                username: "andemad".to_owned(),
+                password: "rask".to_owned(),
+            }],
+            vec![user::Model{
+                id: 1,
+                email: "anders21@student.aau.dk".to_owned(),
+                username: "andemad".to_owned(),
+                password: "rask".to_owned(),},
+                user::Model{
+                    id: 2,
+                    email: "andeand@and.and".to_owned(),
+                    username: "OgsåAndersRask".to_owned(),
+                    password: "rask".to_owned(),
+                }
+            ]
+        ]);
         todo!()
     }
 
