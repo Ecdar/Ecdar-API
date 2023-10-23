c84445a28f8fd525028696b1f931ff43f41a073f
soft-MRJ <mikeriberj@gmail.com>
2023-10-23 11:44:49 +0200

    implemented model_context - delete

--- src/database/model_context.rs
+++ src/database/model_context.rs
@@ -1,8 +1,9 @@
 
 use async_trait::async_trait;
-use sea_orm::DbErr;
-
-use crate::entities::model::Model;
+use sea_orm::{DbErr, Set, ActiveModelTrait, EntityTrait, Unchanged};
+use std::fmt::Display;
+use crate::entities::model::{Model, ActiveModel};
+use crate::entities::prelude::Model as ModelEntity;
 
 use super::{database_context::DatabaseContext, entity_context::EntityContextTrait};
 
@@ -21,23 +22,53 @@ impl EntityContextTrait<Model> for ModelContext {
         ModelContext { db_context }
     }
 
+    /// Used for creating a Model entity
     async fn create(&self, entity: Model) -> Result<Model, DbErr> {
-        todo!()
+        let model = ActiveModel {
+            id: Default::default(),
+            name: Set(entity.name),
+            components_info: Set(entity.components_info),
+            owner_id: Set(entity.owner_id),
+        };
+
+        let model: Model = model.insert(&self.db_context.db).await?;
+        Ok(model)
     }
 
     async fn get_by_id(&self, entity_id: i32) -> Result<Option<Model>, DbErr> {
-        todo!()
+        ModelEntity::find_by_id(entity_id).one(&self.db_context.db).await
     }
 
     async fn get_all(&self) -> Result<Vec<Model>, DbErr> {
-        todo!()
+        ModelEntity::find().all(&self.db_context.db).await
     }
 
     async fn update(&self, entity: Model) -> Result<Model, DbErr> {
-        todo!()
+        let res = &self.get_by_id(entity.id).await?;
+        let updated_model: Result<Model, DbErr> = match res {
+            None => Err(DbErr::RecordNotFound(String::from(format!(
+                "Could not find entity {:?}",
+                entity
+            )))),
+            Some(model) => {
+                ActiveModel {
+                    id: Unchanged(model.id),
+                    name: Set(entity.name),
+                    components_info: Set(entity.components_info),
+                    owner_id: Unchanged(model.id),
+                }
+                .update(&self.db_context.db)
+                .await
+            }
+        };
+        updated_model
     }
 
     async fn delete(&self, entity_id: i32) -> Result<Model, DbErr> {
         todo!()
     }
 }
+
+#[cfg(test)]
+#[path = "../tests/database/model_context.rs"]
+mod tests;
