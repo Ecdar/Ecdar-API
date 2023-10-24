67f8b0efb2483b6a4e63eaae4128e28c6ac37a45
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2023-10-24 14:04:03 +0200

    Documentation for all functions in session_context

--- src/database/session_context.rs
+++ src/database/session_context.rs
@@ -18,12 +18,25 @@ impl SessionContextTrait for SessionContext {}
 
 #[async_trait]
 impl EntityContextTrait<Model> for SessionContext {
+    /// Creates a new `SessionContext` for interacting with the database.
     fn new(db_context: DatabaseContext) -> Self {
         SessionContext {
             db_context: db_context,
         }
     }
-
+    /// Creates a new session in the database based on the provided model.
+    /// # Example
+    /// ```rust
+    /// use crate::entities::session::{Entity, Model};
+    ///
+    /// let new_session = Model {
+    ///         id: 1,
+    ///         token: Uuid::parse_str("4473240f-2acb-422f-bd1a-5214554ed0e0").unwrap(),
+    ///         created_at: Local::now().naive_utc(),
+    ///         user_id,
+    ///     };
+    /// let created_session = session_context.create(model).await.unwrap();
+    /// ```
     async fn create(&self, entity: Model) -> Result<Model, DbErr> {
         let session = ActiveModel {
             id: Default::default(),
@@ -36,14 +49,48 @@ impl EntityContextTrait<Model> for SessionContext {
         session
     }
 
+    /// Returns a session by searching for its id.
+    /// # Example
+    /// ```rust
+    /// let session: Result<Option<Model>, DbErr> = session_context.get_by_id(id).await;
+    /// ```
     async fn get_by_id(&self, id: i32) -> Result<Option<Model>, DbErr> {
         Session::find_by_id(id).one(&self.db_context.db).await
     }
 
+    /// Returns all models in a vector.
+    /// # Example
+    /// ```rust
+    /// let session: Result<Vec<Model>, DbErr> = session_context.get_all().await;
+    /// ```
     async fn get_all(&self) -> Result<Vec<Model>, DbErr> {
         Session::find().all(&self.db_context.db).await
     }
 
+    /// Updates a model in the database based on the provided model.
+    /// # **Example**
+    /// ## ***Model in database***
+    /// ### Model table ###
+    /// | id | token                                | created_at                | user_id |
+    /// |----|--------------------------------------|---------------------------|---------|
+    /// | 1  | 25b14ea1-7b78-4714-b3d0-35d9f56e6cb3 | 2023-09-22T12:42:13+02:00 | 2       |
+    /// ## ***Rust code***
+    /// ```rust
+    /// use crate::entities::session::{Entity, Model};
+    ///
+    /// let new_session = Model {
+    ///         id: 1,
+    ///         token: Uuid::parse_str("4473240f-2acb-422f-bd1a-5214554ed0e0").unwrap(),
+    ///         created_at: Local::now().naive_utc(),
+    ///         user_id: 2,
+    ///     };
+    /// let created_session = session_context.create(model).await.unwrap();
+    /// ```
+    /// ## ***Result***
+    /// ### Model table ###
+    /// | id | token                                | created_at                | user_id |
+    /// |----|--------------------------------------|---------------------------|---------|
+    /// | 1  | 4473240f-2acb-422f-bd1a-5214554ed0e0 | 2023-10-24T13:49:16+02:00 | 2       |
     async fn update(&self, entity: Model) -> Result<Model, DbErr> {
         let res = &self.get_by_id(entity.id).await?;
         let updated_session: Result<Model, DbErr> = match res {
@@ -65,6 +112,22 @@ impl EntityContextTrait<Model> for SessionContext {
         return updated_session;
     }
 
+    /// Deletes a model in the database with a specific id.
+    /// # **Example**
+    /// ## ***Model in database***
+    /// ### Model table ###
+    /// | id | token                                | created_at                | user_id |
+    /// |----|--------------------------------------|---------------------------|---------|
+    /// | 1  | 25b14ea1-7b78-4714-b3d0-35d9f56e6cb3 | 2023-10-24T14:03:37+02:00 | 2       |
+    /// ## ***Rust code***
+    /// ```rust
+    /// let deleted_session = session_context.delete(1).await.unwrap();
+    /// ```
+    /// ## ***Result***
+    /// ### Model table ###
+    /// | id | token | created_at | user_id |
+    /// |----|-------|------------|---------|
+    /// |    |       |            |         |
     async fn delete(&self, id: i32) -> Result<Model, DbErr> {
         let session = self.get_by_id(id).await?;
         match session {
