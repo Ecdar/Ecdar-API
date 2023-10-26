da3df9ffb2c640c009edbed16bde873d836e243d
thor1878 <ttma18@student.aau.dk>
2023-10-26 14:33:17 +0200

    Structured tests for UserContext

--- src/database/session_context.rs
+++ src/database/session_context.rs
@@ -11,7 +11,6 @@ pub struct SessionContext {
     db_context: Box<dyn DatabaseContextTrait>,
 }
 
-
 pub trait SessionContextTrait: EntityContextTrait<Session> {}
 
 impl SessionContextTrait for SessionContext {}
@@ -64,7 +63,9 @@ impl EntityContextTrait<Session> for SessionContext {
     /// let session: Result<Vec<Model>, DbErr> = session_context.get_all().await;
     /// ```
     async fn get_all(&self) -> Result<Vec<Session>, DbErr> {
-        SessionEntity::find().all(&self.db_context.get_connection()).await
+        SessionEntity::find()
+            .all(&self.db_context.get_connection())
+            .await
     }
 
     /// Updates a model in the database based on the provided model.
--- src/tests/database/helpers.rs
+++ src/tests/database/helpers.rs
@@ -1,3 +1,7 @@
+use crate::entities::{
+    model::Model as ModelModel, query::Model as QueryModel, session::Model as SessionModel,
+    user::Model as UserModel,
+};
 use crate::{
     database::{database_context::DatabaseContext, entity_context::EntityContextTrait},
     entities::access::Entity as AccessEntity,
@@ -10,31 +14,15 @@ use crate::{
 use sea_orm::sea_query::TableCreateStatement;
 use sea_orm::{ConnectionTrait, Database, DatabaseBackend, DatabaseConnection, Schema};
 
-///
-///
-/// # Arguments
-///
-/// * `entities`:
-///
-/// returns: DatabaseContext
-///
-/// # Examples
-///
-/// ```
-/// use crate::{entities::user::{Entity as UserEntity}};
-/// // Creates an in-memory sqlite database with the table user
-/// let context : DatabaseContext = setup_db_with_entities(vec!(UserEntity);
-/// ```
-#[allow(dead_code)]
-pub async fn setup_db_with_entities(entities: Vec<AnyEntity>) -> DatabaseContext {
+pub async fn setup_db_with_entities(entities: Vec<AnyEntity>) -> Box<DatabaseContext> {
     let connection = Database::connect("sqlite::memory:").await.unwrap();
     let schema = Schema::new(DatabaseBackend::Sqlite);
     for entity in entities.iter() {
         entity.create_table_from(&connection, &schema).await;
     }
-    DatabaseContext {
+    Box::new(DatabaseContext {
         db_connection: connection,
-    }
+    })
 }
 
 pub enum AnyEntity {
@@ -48,30 +36,30 @@ pub enum AnyEntity {
 
 impl AnyEntity {
     async fn create_table_from(&self, connection: &DatabaseConnection, schema: &Schema) {
-        let mut stmt: TableCreateStatement;
-        match self {
-            AnyEntity::User => {
-                stmt = schema.create_table_from_entity(UserEntity);
-            }
-            AnyEntity::Model => {
-                stmt = schema.create_table_from_entity(ModelEntity);
-            }
-            AnyEntity::Access => {
-                stmt = schema.create_table_from_entity(AccessEntity);
-            }
-            AnyEntity::Session => {
-                stmt = schema.create_table_from_entity(SessionEntity);
-            }
-            AnyEntity::InUse => {
-                stmt = schema.create_table_from_entity(InUseEntity);
-            }
-            AnyEntity::Query => {
-                stmt = schema.create_table_from_entity(QueryEntity);
-            }
-        }
+        let stmt = match self {
+            AnyEntity::User => schema.create_table_from_entity(UserEntity),
+            AnyEntity::Model => schema.create_table_from_entity(ModelEntity),
+            AnyEntity::Access => schema.create_table_from_entity(AccessEntity),
+            AnyEntity::Session => schema.create_table_from_entity(SessionEntity),
+            AnyEntity::InUse => schema.create_table_from_entity(InUseEntity),
+            AnyEntity::Query => schema.create_table_from_entity(QueryEntity),
+        };
         connection
             .execute(connection.get_database_backend().build(&stmt))
             .await
             .unwrap();
     }
+}
+
+pub fn create_users(amount: i32) -> Vec<UserModel> {
+    let mut vector: Vec<UserModel> = vec![];
+    for i in 0..amount {
+        vector.push(UserModel {
+            id: i + 1,
+            email: format!("mail{}@mail.dk", &i),
+            username: format!("username{}", &i),
+            password: format!("qwerty{}", &i),
+        })
+    }
+    vector
 }
