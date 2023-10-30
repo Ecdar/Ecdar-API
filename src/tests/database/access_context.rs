4996ce80e2813e87aea7618ed8e59cf14f83c608
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-10-30 15:54:09 +0100

    added and refactored tests in access_context.rs

--- src/tests/database/access_context.rs
+++ src/tests/database/access_context.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod database_tests {
-    use crate::database::database_context::DatabaseContextTrait;
+    use crate::database::database_context::{DatabaseContext, DatabaseContextTrait};
     use crate::tests::database::helpers::{
         create_accesses, create_models, create_users, setup_db_with_entities, AnyEntity,
     };
@@ -47,22 +47,9 @@ mod database_tests {
     // Test the functionality of the 'create' function, which creates a access in the database
     #[tokio::test]
     async fn create_test() {
-        let db_context =
-            setup_db_with_entities(vec![AnyEntity::User, AnyEntity::Model, AnyEntity::Access])
-                .await;
+        let (_db_context, access_context, access, _, _) = seed_db().await;
 
-        let user_context = UserContext::new(db_context.clone());
-        let model_context = ModelContext::new(db_context.clone());
-        let access_context = AccessContext::new(db_context.clone());
-
-        let new_user = create_users(1)[0].clone();
-        let new_model = create_models(1, new_user.id)[0].clone();
-        let new_access = create_accesses(1, new_user.id, new_model.id)[0].clone();
-
-        // Creates the access in the database using the 'create' function
-        user_context.create(new_user).await.unwrap();
-        model_context.create(new_model).await.unwrap();
-        let created_access = access_context.create(new_access).await.unwrap();
+        let created_access = access_context.create(access).await.unwrap();
 
         let fetched_access = access::Entity::find_by_id(created_access.id)
             .one(&access_context.db_context.get_connection())
@@ -76,60 +63,40 @@ mod database_tests {
 
     #[tokio::test]
     async fn get_by_id_test() -> () {
-        // Setting up a sqlite database in memory to test on
-        let db_context =
-            setup_db_with_entities(vec![AnyEntity::User, AnyEntity::Model, AnyEntity::Access])
-                .await;
-        let user_context = UserContext::new(db_context.clone());
-        let model_context = ModelContext::new(db_context.clone());
-        let access_context = AccessContext::new(db_context.clone());
+        let (db_context, access_context, access, _, _) = seed_db().await;
 
-        let new_user = create_users(1)[0].to_owned();
-        let new_model = create_models(1, new_user.id)[0].clone();
-
-        // Creates a model of the access which will be created
-        let new_access = create_accesses(1, new_user.id, new_model.id)[0].clone();
-
-        // Creates the access in the database using the 'create' function
-        user_context.create(new_user).await.unwrap();
-        model_context.create(new_model).await.unwrap();
-        let created_access = access_context.create(new_access).await.unwrap();
-
-        // Fetches the access created using the 'get_by_id' function
-        let fetched_access = access_context
-            .get_by_id(created_access.id)
-            .await
-            .unwrap()
-            .clone()
-            .unwrap();
-
-        // Assert if the fetched access is the same as the created access
-        assert_eq!(fetched_access, created_access);
-    }
-    #[tokio::test]
-    async fn get_all_test() -> () {
-        // Setting up a sqlite database in memory to test on
-        let db_context =
-            setup_db_with_entities(vec![AnyEntity::User, AnyEntity::Model, AnyEntity::Access])
-                .await;
-        let user_context = UserContext::new(db_context.clone());
-        let model_context = ModelContext::new(db_context.clone());
-        let access_context = AccessContext::new(db_context.clone());
-
-        let new_user = create_users(1)[0].to_owned();
-        let new_model = create_models(1, new_user.id)[0].clone();
-
-        // Creates a model of the access which will be created
-        let new_accesses = create_accesses(3, new_user.id, new_model.id);
-
-        // Creates the access in the database using the 'create' function
-        user_context.create(new_user).await.unwrap();
-        model_context.create(new_model).await.unwrap();
-        access::Entity::insert_many(to_active_models!(new_accesses))
+        access::Entity::insert(access.clone().into_active_model())
             .exec(&db_context.get_connection())
             .await
             .unwrap();
-        assert_eq!(access_context.get_all().await.unwrap().len(), 3)
+
+        // Fetches the access created using the 'get_by_id' function
+        let fetched_access = access_context.get_by_id(access.id).await.unwrap().unwrap();
+
+        // Assert if the fetched access is the same as the created access
+        assert_eq!(access, fetched_access);
+    }
+    #[tokio::test]
+    async fn get_all_test() -> () {
+        let (db_context, access_context, access, user, model) = seed_db().await;
+
+        // Creates a model of the access which will be created
+        let mut new_accesses = create_accesses(3, user.id, model.id);
+
+        // Creates the access in the database using the 'create' function
+        access::Entity::insert_many(to_active_models!(new_accesses.clone()))
+            .exec(&db_context.get_connection())
+            .await
+            .unwrap();
+
+        assert_eq!(access_context.get_all().await.unwrap().len(), 3);
+
+        let mut sorted: Vec<access::Model> = new_accesses.clone();
+        sorted.sort_by_key(|k| k.id);
+
+        for (i, access) in sorted.into_iter().enumerate() {
+            assert_eq!(access, new_accesses[i]);
+        }
     }
 
     #[tokio::test]
@@ -141,24 +108,9 @@ mod database_tests {
             .await
             .unwrap();
 
-        user::Entity::insert(new_user.clone().into_active_model())
-            .exec(&db_context.get_connection())
-            .await
-            .unwrap();
-
-        model::Entity::insert(new_model.clone().into_active_model())
-            .exec(&db_context.get_connection())
-            .await
-            .unwrap();
-
-        access::Entity::insert(new_access.clone().into_active_model())
-            .exec(&db_context.get_connection())
-            .await
-            .unwrap();
-
         let updated_access = access::Model {
             role: Role::Commenter,
-            ..new_access
+            ..access
         };
 
         let updated_access = access_context.update(updated_access.clone()).await.unwrap();
@@ -169,7 +121,7 @@ mod database_tests {
             .unwrap()
             .unwrap();
 
-        assert_eq!(new_access, updated_access);
+        assert_eq!(access, updated_access);
         assert_eq!(updated_access, fetched_access);
     }
 
