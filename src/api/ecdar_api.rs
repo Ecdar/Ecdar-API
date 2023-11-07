41f09afbf46d03ddc7a0c07b7eac3351f8351f16
Mads Risager <madssr2@gmail.com>
2023-11-07 14:02:21 +0100

    Update test path

--- src/api/ecdar_api.rs
+++ src/api/ecdar_api.rs
@@ -215,53 +215,5 @@ impl EcdarBackend for ConcreteEcdarApi {
 }
 
 #[cfg(test)]
-mod ecdar_api {
-    use std::str::FromStr;
-
-    use tonic::{metadata, Request};
-
-    use crate::{api::server::server::ecdar_api_server::EcdarApi, entities::user::Model as User};
-
-    #[tokio::test]
-    async fn delete_user_nonexisting_user_returns_error() -> () {
-        let api = super::ConcreteEcdarApi::setup_in_memory_db().await;
-
-        let mut delete_request = Request::new(super::DeleteUserRequest {
-            token: "ben".to_owned(),
-        });
-
-        // Insert token into request metadata
-        delete_request
-            .metadata_mut()
-            .insert("uid", metadata::MetadataValue::from_str("1").unwrap());
-
-        let delete_response = api.delete_user(delete_request).await;
-
-        assert!(delete_response.is_err());
-    }
-
-    #[tokio::test]
-    async fn delete_user_existing_user_returns_ok() -> () {
-        let api = super::ConcreteEcdarApi::setup_in_memory_db().await;
-
-        let user = User {
-            id: 1,
-            email: "anders21@student.aau.dk".to_string(),
-            username: "anders".to_string(),
-            password: "123".to_string(),
-        };
-
-        let mut delete_request = Request::new(super::DeleteUserRequest {
-            token: "shut ur ass up".to_owned(),
-        });
-
-        // Insert token into request metadata
-        delete_request
-            .metadata_mut()
-            .insert("uid", metadata::MetadataValue::from_str("1").unwrap());
-
-        let delete_response = api.delete_user(delete_request).await;
-
-        assert!(delete_response.is_err());
-    }
-}
+#[path = "../tests/api/ecdar_api.rs"]
+mod tests;
