ed7c3533a83627bf4a678a6f30cb64b513e32fcc
sabotack <sabotack.2001@gmail.com>
2023-11-07 13:41:46 +0100

    Add test for delete user endpoint

--- src/api/ecdar_api.rs
+++ src/api/ecdar_api.rs
@@ -14,6 +14,8 @@ use crate::database::query_context::QueryContext;
 use crate::database::session_context::SessionContext;
 use crate::database::user_context::UserContext;
 
+use self::helpers::helpers::{setup_db_with_entities, AnyEntity};
+
 use super::{
     auth,
     server::server::{
@@ -22,6 +24,9 @@ use super::{
         SimulationStartRequest, SimulationStepRequest, SimulationStepResponse, UserTokenResponse,
     },
 };
+
+#[path = "../tests/database/helpers.rs"]
+pub mod helpers;
 
 #[derive(Debug)]
 pub struct ConcreteEcdarApi {
@@ -49,7 +54,15 @@ impl ConcreteEcdarApi {
             in_use_context: Box::new(InUseContext::new(db_context.clone())),
         }
     }
+
+    pub async fn setup_in_memory_db() -> Self {
+        let db_context = setup_db_with_entities(vec![AnyEntity::User, AnyEntity::Model]).await;
+        
+        env::set_var("REVEAAL_ADDRESS", "");
+        ConcreteEcdarApi::new(Box::new(db_context)).await
+    }
 }
+
 
 #[tonic::async_trait]
 impl EcdarApi for ConcreteEcdarApi {
@@ -180,5 +193,60 @@ impl EcdarBackend for ConcreteEcdarApi {
             .await
             .unwrap();
         client.take_simulation_step(request).await
+    }
+}
+
+
+#[cfg(test)]
+mod ecdar_api {
+    use std::str::FromStr;
+
+    use tonic::{Request, metadata};
+
+    use crate::{api::server::server::ecdar_api_server::EcdarApi, entities::user::Model};
+
+    #[tokio::test]
+    async fn delete_user_nonexisting_user_returns_error() -> () {
+        let api = super::ConcreteEcdarApi::setup_in_memory_db().await;
+
+        let mut delete_request = Request::new(super::DeleteUserRequest {
+            token: "ben".to_owned(),
+        });
+
+        // Insert token into request metadata
+        delete_request.metadata_mut().insert(
+            "uid",
+            metadata::MetadataValue::from_str("1").unwrap(),
+        );
+
+        let delete_response = api.delete_user(delete_request).await;
+
+        assert!(delete_response.is_err());
+    }
+
+    #[tokio::test]
+    async fn delete_user_existing_user_returns_ok() -> () {
+        let api = super::ConcreteEcdarApi::setup_in_memory_db().await;
+
+        let user = Model {
+            id: 1,
+            email: "anders21@student.aau.dk".to_string(),
+            username: "anders".to_string(),
+            password: "123".to_string(),
+        };
+
+        let mut delete_request = Request::new(super::DeleteUserRequest {
+            token: "shut ur ass up".to_owned(),
+        });
+
+        // Insert token into request metadata
+        delete_request.metadata_mut().insert(
+            "uid",
+            metadata::MetadataValue::from_str("1").unwrap(),
+        );
+
+        let delete_response = api.delete_user(delete_request).await;
+
+        assert!(delete_response.is_err());
     }
 }
