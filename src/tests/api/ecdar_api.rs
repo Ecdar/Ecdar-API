e82df9f328abdefd6504fe250becf32f3a005986
Mads Risager <madssr2@gmail.com>
2023-11-08 10:14:18 +0100

    Add create_user tests

--- src/tests/api/ecdar_api.rs
+++ src/tests/api/ecdar_api.rs
@@ -1,16 +1,21 @@
 #[cfg(test)]
 mod ecdar_api {
+    use std::env;
     use std::str::FromStr;
-
     use tonic::{metadata, Request};
-
-    use crate::{api::server::server::ecdar_api_server::EcdarApi, entities::user::Model as User};
+    use crate::api::ecdar_api::helpers::helpers::AnyEntity;
     use crate::api::ecdar_api::ConcreteEcdarApi;
-    use crate::api::server::server::DeleteUserRequest;
+    use crate::api::server::server::ecdar_api_auth_server::EcdarApiAuth;
+    use crate::api::server::server::{CreateUserRequest, DeleteUserRequest};
+    use crate::database::entity_context::EntityContextTrait;
+    use crate::{
+        api::server::server::ecdar_api_server::EcdarApi, entities::user::Entity as UserEntity,
+        entities::user::Model as User,
+    };
 
     #[tokio::test]
-    async fn delete_user_nonexisting_user_returns_error() -> () {
-        let api = ConcreteEcdarApi::setup_in_memory_db().await;
+    async fn delete_user_nonexistent_user_returns_error() -> () {
+        let api = ConcreteEcdarApi::setup_in_memory_db(vec![AnyEntity::User]).await;
 
         let mut delete_request = Request::new(DeleteUserRequest {
             token: "ben".to_owned(),
@@ -28,7 +33,7 @@ mod ecdar_api {
 
     #[tokio::test]
     async fn delete_user_existing_user_returns_ok() -> () {
-        let api = ConcreteEcdarApi::setup_in_memory_db().await;
+        let api = ConcreteEcdarApi::setup_in_memory_db(vec![AnyEntity::User]).await;
 
         let user = User {
             id: 1,
@@ -49,5 +54,44 @@ mod ecdar_api {
         let delete_response = api.delete_user(delete_request).await;
 
         assert!(delete_response.is_err());
+    }
+
+    #[tokio::test]
+    async fn create_user_returns_token() -> () {
+        let api = ConcreteEcdarApi::setup_in_memory_db(vec![AnyEntity::User]).await;
+
+        let create_user_request = Request::new(CreateUserRequest {
+            email: "anders21@student.aau.dk".to_string(),
+            username: "anders".to_string(),
+            password: "123".to_string(),
+        });
+
+        let create_user_response = api.create_user(create_user_request).await;
+        assert!(create_user_response.is_ok())
+    }
+
+    #[tokio::test]
+    async fn create_user_existing_user_returns_err() -> () {
+        let api = ConcreteEcdarApi::setup_in_memory_db(vec![AnyEntity::User]).await;
+
+        let _ = api
+            .user_context
+            .create(User {
+                id: Default::default(),
+                email: "anders21@student.aau.dk".to_string(),
+                username: "anders".to_string(),
+                password: "123".to_string(),
+            })
+            .await
+            .unwrap();
+
+        let create_user_request = Request::new(CreateUserRequest {
+            email: "anders21@student.aau.dk".to_string(),
+            username: "anders".to_string(),
+            password: "123".to_string(),
+        });
+
+        let create_user_response = api.create_user(create_user_request).await;
+        assert!(create_user_response.is_err())
     }
 }
