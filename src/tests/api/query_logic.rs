940df53149be869ef988b61b1a1b86b04100dc2b
AlexanderManich <alexfredberg@gmail.com>
2023-12-05 09:07:00 +0100

    fixed tests

--- src/tests/api/query_logic.rs
+++ src/tests/api/query_logic.rs
@@ -1,10 +1,12 @@
+use std::str::FromStr;
+
 use crate::api::server::server::ecdar_api_server::EcdarApi;
 use crate::api::server::server::{CreateQueryRequest, DeleteQueryRequest, UpdateQueryRequest};
-use crate::entities::query;
+use crate::entities::{access, query};
 use crate::tests::api::helpers::{get_mock_concrete_ecdar_api, get_mock_services};
 use mockall::predicate;
 use sea_orm::DbErr;
-use tonic::{Code, Request};
+use tonic::{metadata, Code, Request};
 
 #[tokio::test]
 async fn create_invalid_query_returns_err() {
@@ -18,16 +20,33 @@ async fn create_invalid_query_returns_err() {
         outdated: Default::default(),
     };
 
+    let access = access::Model {
+        id: Default::default(),
+        role: "Editor".to_string(),
+        model_id: 1,
+        user_id: 1,
+    };
+
+    mock_services
+        .access_context_mock
+        .expect_get_access_by_uid_and_model_id()
+        .with(predicate::eq(1), predicate::eq(1))
+        .returning(move |_, _| Ok(Some(access.clone())));
+
     mock_services
         .query_context_mock
         .expect_create()
         .with(predicate::eq(query.clone()))
         .returning(move |_| Err(DbErr::RecordNotInserted));
 
-    let request = Request::new(CreateQueryRequest {
+    let mut request = Request::new(CreateQueryRequest {
         string: "".to_string(),
         model_id: 1,
     });
+
+    request
+        .metadata_mut()
+        .insert("uid", metadata::MetadataValue::from_str("1").unwrap());
 
     let api = get_mock_concrete_ecdar_api(mock_services);
 
@@ -48,16 +67,33 @@ async fn create_query_returns_ok() {
         outdated: Default::default(),
     };
 
+    let access = access::Model {
+        id: Default::default(),
+        role: "Editor".to_string(),
+        model_id: 1,
+        user_id: 1,
+    };
+
+    mock_services
+        .access_context_mock
+        .expect_get_access_by_uid_and_model_id()
+        .with(predicate::eq(1), predicate::eq(1))
+        .returning(move |_, _| Ok(Some(access.clone())));
+
     mock_services
         .query_context_mock
         .expect_create()
         .with(predicate::eq(query.clone()))
         .returning(move |_| Ok(query.clone()));
 
-    let request = Request::new(CreateQueryRequest {
+    let mut request = Request::new(CreateQueryRequest {
         string: "".to_string(),
         model_id: 1,
     });
+
+    request
+        .metadata_mut()
+        .insert("uid", metadata::MetadataValue::from_str("1").unwrap());
 
     let api = get_mock_concrete_ecdar_api(mock_services);
 
@@ -83,6 +119,13 @@ async fn update_invalid_query_returns_err() {
         ..old_query.clone()
     };
 
+    let access = access::Model {
+        id: 1,
+        role: "Editor".to_string(),
+        model_id: Default::default(),
+        user_id: 1,
+    };
+
     mock_services
         .query_context_mock
         .expect_get_by_id()
@@ -90,15 +133,25 @@ async fn update_invalid_query_returns_err() {
         .returning(move |_| Ok(Some(old_query.clone())));
 
     mock_services
+        .access_context_mock
+        .expect_get_access_by_uid_and_model_id()
+        .with(predicate::eq(1), predicate::eq(0))
+        .returning(move |_, _| Ok(Some(access.clone())));
+
+    mock_services
         .query_context_mock
         .expect_update()
         .with(predicate::eq(query.clone()))
         .returning(move |_| Err(DbErr::RecordNotUpdated));
 
-    let request = Request::new(UpdateQueryRequest {
+    let mut request = Request::new(UpdateQueryRequest {
         id: 1,
         string: "updated".to_string(),
     });
+
+    request
+        .metadata_mut()
+        .insert("uid", metadata::MetadataValue::from_str("1").unwrap());
 
     let api = get_mock_concrete_ecdar_api(mock_services);
 
@@ -124,6 +177,19 @@ async fn update_query_returns_ok() {
         ..old_query.clone()
     };
 
+    let access = access::Model {
+        id: Default::default(),
+        role: "Editor".to_string(),
+        model_id: Default::default(),
+        user_id: 1,
+    };
+
+    mock_services
+        .access_context_mock
+        .expect_get_access_by_uid_and_model_id()
+        .with(predicate::eq(1), predicate::eq(0))
+        .returning(move |_, _| Ok(Some(access.clone())));
+
     mock_services
         .query_context_mock
         .expect_get_by_id()
@@ -136,10 +202,14 @@ async fn update_query_returns_ok() {
         .with(predicate::eq(query.clone()))
         .returning(move |_| Ok(query.clone()));
 
-    let request = Request::new(UpdateQueryRequest {
+    let mut request = Request::new(UpdateQueryRequest {
         id: 1,
         string: "updated".to_string(),
     });
+
+    request
+        .metadata_mut()
+        .insert("uid", metadata::MetadataValue::from_str("1").unwrap());
 
     let api = get_mock_concrete_ecdar_api(mock_services);
 
@@ -152,13 +222,44 @@ async fn update_query_returns_ok() {
 async fn delete_invalid_query_returns_err() {
     let mut mock_services = get_mock_services();
 
+    let access = access::Model {
+        id: Default::default(),
+        role: "Editor".to_string(),
+        model_id: Default::default(),
+        user_id: 1,
+    };
+
+    let query = query::Model {
+        id: 1,
+        string: "".to_string(),
+        result: Default::default(),
+        model_id: Default::default(),
+        outdated: Default::default(),
+    };
+
+    mock_services
+        .access_context_mock
+        .expect_get_access_by_uid_and_model_id()
+        .with(predicate::eq(1), predicate::eq(0))
+        .returning(move |_, _| Ok(Some(access.clone())));
+
+    mock_services
+        .query_context_mock
+        .expect_get_by_id()
+        .with(predicate::eq(1))
+        .returning(move |_| Ok(Some(query.clone())));
+
     mock_services
         .query_context_mock
         .expect_delete()
         .with(predicate::eq(1))
         .returning(move |_| Err(DbErr::RecordNotFound("".to_string())));
 
-    let request = Request::new(DeleteQueryRequest { id: 1 });
+    let mut request = Request::new(DeleteQueryRequest { id: 1 });
+
+    request
+        .metadata_mut()
+        .insert("uid", metadata::MetadataValue::from_str("1").unwrap());
 
     let api = get_mock_concrete_ecdar_api(mock_services);
 
@@ -179,13 +280,38 @@ async fn delete_query_returns_ok() {
         outdated: Default::default(),
     };
 
+    let query_clone = query.clone();
+
+    let access = access::Model {
+        id: Default::default(),
+        role: "Editor".to_string(),
+        model_id: Default::default(),
+        user_id: 1,
+    };
+
+    mock_services
+        .query_context_mock
+        .expect_get_by_id()
+        .with(predicate::eq(1))
+        .returning(move |_| Ok(Some(query.clone())));
+
+    mock_services
+        .access_context_mock
+        .expect_get_access_by_uid_and_model_id()
+        .with(predicate::eq(1), predicate::eq(0))
+        .returning(move |_, _| Ok(Some(access.clone())));
+
     mock_services
         .query_context_mock
         .expect_delete()
         .with(predicate::eq(1))
-        .returning(move |_| Ok(query.clone()));
+        .returning(move |_| Ok(query_clone.clone()));
 
-    let request = Request::new(DeleteQueryRequest { id: 1 });
+    let mut request = Request::new(DeleteQueryRequest { id: 1 });
+
+    request
+        .metadata_mut()
+        .insert("uid", metadata::MetadataValue::from_str("1").unwrap());
 
     let api = get_mock_concrete_ecdar_api(mock_services);
 
