9e3d85f87bd0b41055becf55afd095cdd666d532
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-12-11 12:47:13 +0100

    moved doc comments from impl to trait

--- src/controllers/controller_traits/session_controller_trait.rs
+++ src/controllers/controller_traits/session_controller_trait.rs
@@ -4,7 +4,17 @@ use tonic::{Request, Response, Status};
 
 #[async_trait]
 pub trait SessionControllerTrait: Send + Sync {
+    /// Deletes the requester's session, found by their access token.
+    ///  
+    /// Returns the response that is received from Reveaal.
     async fn delete_session(&self, _request: Request<()>) -> Result<Response<()>, Status>;
+
+    /// This method is used to get a new access and refresh token for a user.
+    ///
+    /// # Errors
+    /// This function will return an error if the user does not exist in the contexts,
+    /// if the password in the request does not match the user's password,
+    /// or if no user is provided in the request.
     async fn get_auth_token(
         &self,
         request: Request<GetAuthTokenRequest>,
