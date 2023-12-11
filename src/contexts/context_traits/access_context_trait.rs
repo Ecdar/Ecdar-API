89e1ac97145f07457b16c796fc13a4f703e66ee5
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-12-11 10:01:09 +0100

    Documented access context trait

--- src/contexts/context_traits/access_context_trait.rs
+++ src/contexts/context_traits/access_context_trait.rs
@@ -6,11 +6,15 @@ use sea_orm::DbErr;
 
 #[async_trait]
 pub trait AccessContextTrait: EntityContextTrait<access::Model> {
+    /// Searches for an access entity by `User` and `Project` id, 
+    /// returning [`Some`] if any entity was found, [`None`] otherwise
+    /// # Errors
+    /// Errors on failed connection, execution error or constraint violations.
     async fn get_access_by_uid_and_project_id(
         &self,
         uid: i32,
         project_id: i32,
     ) -> Result<Option<access::Model>, DbErr>;
-
+    /// Returns all [`access::Model`] that are associated with a given `Project``
     async fn get_access_by_project_id(&self, project_id: i32) -> Result<Vec<AccessInfo>, DbErr>;
 }
