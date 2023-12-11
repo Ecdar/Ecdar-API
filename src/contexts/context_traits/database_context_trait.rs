a750f5a635078c7875b39525e9230887cda01ed0
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2023-12-11 10:07:19 +0100

    Documented database context trait

--- src/contexts/context_traits/database_context_trait.rs
+++ src/contexts/context_traits/database_context_trait.rs
@@ -5,6 +5,10 @@ use std::sync::Arc;
 
 #[async_trait]
 pub trait DatabaseContextTrait: Send + Sync + Debug {
+    /// Resets the database, usually by truncating all tables, useful for testing on a single database
+    /// # Errors
+    /// Errors on failed connection or execution error.
     async fn reset(&self) -> Result<Arc<dyn DatabaseContextTrait>, DbErr>;
+    /// Gets the connection to the database
     fn get_connection(&self) -> DatabaseConnection;
 }
